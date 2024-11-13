9db2bff7672c6c007bf7757f3ab91f408270b2b0
Payson Eberz <63822744+PAYS0N@users.noreply.github.com>
2024-11-12 22:51:28 -0600

    Set up file structure

---
+++ lib/binary_search_tree.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module OdinBinarySearchTree
+  # Implementation of binary search tree
+  class BinarySearchTree
+    def initialize
+      puts "initialized"
+    end
+  end
+end
---
+++ lib/odin_binary_search_tree.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+require_relative("binary_search_tree")
+
+tree = OdinBinarySearchTree::BinarySearchTree.new
