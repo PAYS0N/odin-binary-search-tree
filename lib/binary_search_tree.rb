2c639787f787ff196d5d3d1bce0752268b4bcb00
Payson Eberz <63822744+PAYS0N@users.noreply.github.com>
2024-11-13 20:23:51 -0600

    Add method to create the initially balanced tree

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,10 +1,37 @@
 # frozen_string_literal: true
 
+require_relative("node")
+
 module OdinBinarySearchTree
   # Implementation of binary search tree
   class BinarySearchTree
-    def initialize
-      puts "initialized"
+    attr_reader :root
+
+    def initialize(arr)
+      arr = clean(arr)
+      @root = build_tree(arr)
+    end
+
+    def build_tree(arr)
+      return nil if arr == []
+
+      left = arr[0...arr.length / 2]
+      right = arr[(arr.length / 2) + 1..]
+      root = OdinBinarySearchTree::Node.new(arr[arr.length / 2])
+      root.left = build_tree(left)
+      root.right = build_tree(right)
+      root
+    end
+
+    def clean(arr)
+      arr.sort.uniq
+    end
+
+    # print function from unknown fellow student
+    def pretty_print(node = @root, prefix = "", is_left = true)
+      pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+      puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.val}"
+      pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
     end
   end
 end
--- lib/odin_binary_search_tree.rb
+++ lib/odin_binary_search_tree.rb
@@ -2,4 +2,7 @@
 
 require_relative("binary_search_tree")
 
-tree = OdinBinarySearchTree::BinarySearchTree.new
+tree = OdinBinarySearchTree::BinarySearchTree.new([1, 2, 3, 4, 5, 6, 7, 8])
+tree.pretty_print
+tree = OdinBinarySearchTree::BinarySearchTree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+tree.pretty_print
